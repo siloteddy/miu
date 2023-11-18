c51f7388070434cfcc01a7639bdfc706b2bfe473
shilorya <doknes@mail.ru>
2023-11-18 19:01:52 +0300

    qw

---
+++ qwe.py
@@ -0,0 +1,36 @@
+# task 1(C)
+# q = [12, 54, 454, 3, 871, 666]
+# w = [666, 3, 3432, 11, 43]
+
+# numbers = sorted(list(set(q) & set(w)))
+
+# for number in numbers:
+#     print(number)
+
+# task 2(F)
+
+# with open('input.txt', 'r') as qwe:
+#     w = qwe.read()
+
+# words = w.split()
+# q = set(words)
+# count = len(q)
+
+# print(count)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
